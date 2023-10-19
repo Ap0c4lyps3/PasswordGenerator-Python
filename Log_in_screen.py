f2b9e55748e189e3af13e32a5cc9495cb9362668
Ap0c4lyps3 <140135036+Ap0c4lyps3@users.noreply.github.com>
2023-10-18 21:19:28 -0600

    Add files via upload

---
+++ Log_in_screen.py
@@ -0,0 +1,66 @@
+import customtkinter as ctk
+from PIL import Image, ImageTk
+
+#set background
+ctk.set_appearance_mode("light")
+
+# Create the application window
+root = ctk.CTk()
+root.title("Display")
+root.geometry("600x600")
+root.state("normal")
+
+def login():
+    print("Log in successfully")
+
+def rememberUser():
+    print("Remembered in the system")
+
+def faceID_Login():
+    print("Using FaceID")
+
+#create big frame
+frame = ctk.CTkFrame(master=root)
+frame.pack(pady=100, padx=10, expand=True)
+
+#create name of page
+label = ctk.CTkLabel(master=frame, text="Log in", font=("Arial", 24), text_color="#117ACA")
+label.pack(pady=12, padx=10)
+
+#create username box
+entry1 = ctk.CTkEntry(master=frame, placeholder_text="Enter your User ID")
+entry1.pack(pady=12, padx=10)
+#create password box
+entry2 = ctk.CTkEntry(master=frame, placeholder_text="Enter your Password", show="*")
+entry2.pack(pady=12, padx=10)
+try:
+    with open('passwords.txt', 'a') as f:
+        # Get the text from the entries and write to the file
+        f.write(entry1.get() + "|" + entry2.get() + "\n")
+        print("Successfully added to the system")
+except Exception as e:
+    print("An error has occurred")
+finally:
+    # This block will always execute, close the file if it's open
+    f.close()
+
+#create remember me checkbox
+checkbox = ctk.CTkCheckBox(master=frame, text="Remember Me", command=rememberUser)
+checkbox.pack(pady=12, padx=10)
+
+#create use token checkbox
+button = ctk.CTkCheckBox(master=frame, text="Face ID", command=faceID_Login)
+button.pack(pady=12, padx=80)
+
+#create log in button
+button1 = ctk.CTkButton(master=frame, text="Log On", command=login)
+button1.pack(pady=30, padx=10)
+
+#Run the application
+root.mainloop()
+
+
+
+
+
+
